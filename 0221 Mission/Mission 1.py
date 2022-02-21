3b0394681176d29181c1232935aecc536e87e5c1
ordinaryhan <ordinaryhan@naver.com>
2022-02-21 16:44:16 +0900

    time_to_fstr 수정

--- 0221 Mission/Mission 1.py
+++ 0221 Mission/Mission 1.py
@@ -13,7 +13,6 @@
         - 변환 함수를 이용한 시간 정보 출력 함수 1개
 """
 
-import time
 from datetime import datetime, timezone, timedelta
 
 
@@ -24,15 +23,17 @@ def utc_to_kst(t: datetime) -> datetime:
 
 
 # datetime -> Format String 에 맞춰 변환
+# strftime 함수는 '%_'(방식1) 문자열을 인식해 시간 정보를 변환한다.
+# 하지만 가독성은 'yyyy-mm-dd HH:MM:SS'(방식2) 가 더 높으므로
+# 함수에 넣는 문자열 형식 파라미터는 방식2를 사용하고
+# 함수에서 방식1로 바꾸어 strftime 함수로 시간 정보를 변환하도록 한다.
 def time_to_fstr(t: datetime, fmt: str) -> str:
-    fmt = fmt.replace('YYYY', '%Y')
-    fmt = fmt.replace('yy', '%y')
-    fmt = fmt.replace('mm', '%m')     # 월
-    fmt = fmt.replace('dd', '%d')     # 일
-    fmt = fmt.replace('ll', '%l')     # 12시간제 시간
-    fmt = fmt.replace('HH', '%H')     # 24시간제 시간
-    fmt = fmt.replace('MM', '%M')     # 분
-    fmt = fmt.replace('SS', '%S')     # 초
+    trans_table = {'YYYY': '%Y', 'yy': '%y',    # 년도
+                   'mm': '%m', 'dd': '%d',      # 월 / 일
+                   'll': '%l', 'HH': '%H',      # 12시간제 / 24시간제
+                   'MM': '%M', 'SS': '%S'}      # 분 / 초
+    for key in trans_table:
+        fmt = fmt.replace(key, trans_table[key])
     return t.strftime(fmt)
 
 
@@ -64,7 +65,7 @@ if __name__ == '__main__':
     now = datetime.now(timezone.utc)
     # 테스트할 포맷들
     ftz = [('YYYY-mm-dd HH:MM:SS', 'KST'), ('yy년 mm월 dd일', 'error'),
-         ('현재 MM분 SS초 입니다.', 'UTC'), ('mm 월 MM 분', 'kst'), ('-', 'utc')]
+           ('현재 MM분 SS초 입니다.', 'UTC'), ('mm 월 MM 분', 'kst'), ('-', 'utc')]
     # 현재 시간 정보(UTC 기준)를 지정된 시간대, 출력 포맷으로 변환
     for i in range(len(ftz)):
         print(f"[{i}]", trans_time(now, ftz[i][0], ftz[i][1]))
