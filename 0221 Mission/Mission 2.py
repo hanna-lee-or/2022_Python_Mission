65972f2c691815ceae4c0378ca4a059944224af5
ordinaryhan <ordinaryhan@naver.com>
2022-02-21 17:58:28 +0900

    정규식 수정

--- 0221 Mission/Mission 2.py
+++ 0221 Mission/Mission 2.py
@@ -1,4 +1,3 @@
-
 """ [Mission 2. 이메일 형식 체크 함수 만들기] Hanna 22/02/21
 
     - check_email_fmt 함수 : 이메일 형식 유효성 체크
@@ -14,10 +13,9 @@ import re
 # 이메일은 계정@도메인.최상위도메인 형식. 계정에 [+-_.] 등의 문자가 들어갈 수 있음.
 # 도메인에 [-] 문자를 사용할 수 있고, 최상위 도메인이 여러 단계일 수 있음.
 def check_email_fmt(email: str) -> bool:
-
-    regex = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    valid = re.match(regex, email)
-    print(valid)
+    regex = '([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+' \
+            '@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
+    valid = re.fullmatch(regex, email)
     if not valid:
         return False
     else:
@@ -26,8 +24,7 @@ def check_email_fmt(email: str) -> bool:
 
 # 테스트 코드
 if __name__ == '__main__':
-    emails = ["email@email.com", "email@email..com"]
+    emails = ["email@email.com", "email@email..com",
+              "em.a.il@email.co.kr", "dan@[128.6.3.40]"]
     for e in emails:
         print(check_email_fmt(e))
-
-
