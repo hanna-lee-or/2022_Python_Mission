3a13f501f2b5e7c4199b37ce99cb446bb308725b
ordinaryhan <ordinaryhan@naver.com>
2022-02-22 09:15:12 +0900

    주석 제거. Docstring 추가.

--- 0221 Mission/Mission 2.py
+++ 0221 Mission/Mission 2.py
@@ -1,9 +1,8 @@
 """ [Mission 2. 이메일 형식 체크 함수 만들기] Hanna 22/02/21
 
-    - check_email_fmt 함수 : 이메일 형식 유효성 체크
+    - 목표 : 이메일 형식 유효성 체크
 
-    [1] check_email_fmt 함수 파라미터
-        - 'email: str' : 형식을 체크할 이메일
+    [1] check_email_fmt 함수
         - return : 유효성 여부 True/False 반환
 """
 
@@ -13,6 +12,7 @@ import re
 # 이메일은 계정@도메인.최상위도메인 형식. 계정에 [+-_.] 등의 문자가 들어갈 수 있음.
 # 도메인에 [-] 문자를 사용할 수 있고, 최상위 도메인이 여러 단계일 수 있음.
 def check_email_fmt(email: str) -> bool:
+    """ 이메일 형식 유효성 체크 """
     regex = '([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+' \
             '@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'
     valid = re.fullmatch(regex, email)
@@ -26,5 +26,5 @@ def check_email_fmt(email: str) -> bool:
 if __name__ == '__main__':
     emails = ["email@email.com", "email@email..com",
               "em.a.il@email.co.kr", "dan@[128.6.3.40]"]
-    for e in emails:
-        print(check_email_fmt(e))
+    for i, e in enumerate(emails):
+        print(f"[{i}]", check_email_fmt(e))
