37ea9373780b1c78ca9845f7d970f4336dc43e14
ordinaryhan <ordinaryhan@naver.com>
2022-02-22 13:48:02 +0900

    큰 틀만 제작

--- 0221 Mission/Mission 3.py
+++ 0221 Mission/Mission 3.py
@@ -10,21 +10,25 @@
         - return : 성공 여부
 """
 
+# pip install pymysql, pandas
+import pandas as pd
+import pymysql
+
 
 def xlsx_to_db(fname: str) -> bool:
     """ fname.xlsx 엑셀 파일을 읽어서 DB 테이블에 저장 """
-    pass
+
     return True
 
 
 def db_to_xlsx(fname: str) -> bool:
     """ DB 테이블을 읽어서 fname.xlsx 엑셀 파일에 저장 """
-    pass
+
     return True
 
 
 # 테스트 코드
 if __name__ == '__main__':
     fnames = ("서울도서관 분야별 장서현황_지식문화과", "DB 장서현황")
-    print(f"[{fnames[0]} -> DB]", xlsx_to_db(fnames))
-    print(f"[DB -> {fnames[1]}]", db_to_xlsx(fnames))
+    print(f"[{fnames[0]} -> DB]", xlsx_to_db(fnames[0]))
+    print(f"[DB -> {fnames[1]}]", db_to_xlsx(fnames[1]))
